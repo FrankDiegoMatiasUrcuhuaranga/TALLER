f17423b0cc2e5a13545c05b79d06dc97e8614ea3
FrankDiegoMatiasUrcuhuaranga <T00049K@ms.upla.edu.pe>
2025-09-22 01:45:56 -0500

    Update app_docenas.java

--- CODIGOS/SEMANA 2/EJERCICIO 1/app_docenas.java
+++ CODIGOS/SEMANA 2/EJERCICIO 1/app_docenas.java
@@ -1,7 +1,3 @@
-/**
- * Clase principal para ejecutar el sistema de descuentos por docenas con lapiceros
- * @author Estudiante UPLA
- */
 public class APP_DOCENAS {
     public static void main(String[] args) {
         EJERCICIO3_DOCENAS docenas = new EJERCICIO3_DOCENAS();
